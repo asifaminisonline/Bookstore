59e2b73e1c6b8f3e05c2cf1cb0c13e4b0f9c34ae
asifaminisonline <asifaminisonline@gmail.com>
2023-01-24 13:46:20 +0530

    Use Redux in React Components

--- src/App.js
+++ src/App.js
@@ -5,24 +5,12 @@ import NavBar from './components/NavBar';
 import Books from './components/Books';
 import Categories from './components/Categories';
 
-const books = [
-  {
-    id: 1,
-    genre: 'genre',
-    title: 'book title',
-    author: 'author',
-    status: 'complete',
-    currentChapter: 'Current chapter',
-    chapterNumber: 'Chapter #number',
-  },
-];
-
 export default function App() {
   return (
     <div>
       <NavBar />
       <Routes>
-        <Route exact path="/" element={<Books books={books} />} />
+        <Route exact path="/" element={<Books />} />
         <Route exact path="/categories" element={<Categories />} />
       </Routes>
     </div>
